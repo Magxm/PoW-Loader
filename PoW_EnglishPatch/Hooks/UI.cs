63a82eeb56f5dd7cd64815c2cce388e3e509952e
Magxm <19425588+Magxm@users.noreply.github.com>
2021-02-06 03:09:49 +0100

    Fixed UI Hooks

--- PoW_EnglishPatch/Hooks/UI.cs
+++ PoW_EnglishPatch/Hooks/UI.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Reflection;
+﻿using System.Reflection;
 
 using HarmonyLib;
 
@@ -35,7 +34,6 @@ namespace EnglishPatch.Hooks
         }
     }
 
-
     [HarmonyPatch(typeof(WGStringTable), "UpdateString")]
     public class WGStringTable_UpdateString_Hook
     {
@@ -54,12 +52,9 @@ namespace EnglishPatch.Hooks
         }
     }
 
-
     [HarmonyPatch(typeof(UIBattleStatus), "UpdateView")]
     public class UIBattleStatus_UpdateView_Hook
     {
-
-
         public static void Postfix(ref UIBattleStatus __instance)
         {
             __instance.FinalCounterAttack.GetComponent<Text>().horizontalOverflow = HorizontalWrapMode.Overflow;
@@ -70,12 +65,9 @@ namespace EnglishPatch.Hooks
         }
     }
 
-
     [HarmonyPatch(typeof(UIQuest), "SetTrackedQuest")]
     public class UIQuest_SetTrackedQuest_Hook
     {
-
-
         public static void Postfix(ref UIQuest __instance, ref WGText ___questName, ref WGText ___description)
         {
             ___questName.GetComponent<Text>().fontSize = 18;
@@ -83,12 +75,9 @@ namespace EnglishPatch.Hooks
         }
     }
 
-
     [HarmonyPatch(typeof(WGBattleInfo), "UpdateInfo")]
     public class WGBattleInfo_UpdateInfo_Hook
     {
-
-
         public static void Postfix(ref WGBattleInfo __instance, ref WGText ___Name)
         {
             ___Name.GetComponent<Text>().font = Resources.GetBuiltinResource<Font>("Arial.ttf");
@@ -96,24 +85,18 @@ namespace EnglishPatch.Hooks
         }
     }
 
-
     [HarmonyPatch(typeof(WGPaoMaDengText), "AddMessage")]
     public class WGPaoMaDengText_AddMessage_Hook
     {
-
-
         public static void Postfix(ref WGPaoMaDengText __instance, ref WGText ___pmdText)
         {
             ___pmdText.GetComponent<Text>().font = Resources.GetBuiltinResource<Font>("Arial.ttf");
         }
     }
 
-
     [HarmonyPatch(typeof(WGNurturanceBtn), "UpdateButton")]
     public class WGNurturanceBtn_UpdateButton_Hook
     {
-
-
         public static void Postfix(ref WGNurturanceBtn __instance, ref WGText ___Btn_Name)
         {
             __instance.Btn_Name.GetComponent<Text>().font = Resources.GetBuiltinResource<Font>("Arial.ttf");
@@ -125,27 +108,21 @@ namespace EnglishPatch.Hooks
     [HarmonyPatch(typeof(UIRegistration), "NameCheck")]
     public class UIRegistration_NameCheck_Hook
     {
-
-
         public static bool Prefix()
         {
             return false;
         }
     }
 
-
     [HarmonyPatch(typeof(SteamPlatform), "CheckWordLegality")]
     public class SteamPlatform_CheckWordLegality_Hook
     {
-
-
         public static bool Prefix(string word, ref bool __result)
         {
             __result = true;
             return false;
         }
     }
-
 
     [HarmonyPatch(typeof(WGBookBtn), "UpdateWidget")]
     public class WGBookBtn_UpdateWidget_Hook
@@ -167,8 +144,6 @@ namespace EnglishPatch.Hooks
     [HarmonyPatch(typeof(WGTraitBtn), "UpdateWidget")]
     public class WGTraitBtn_UpdateWidget_Hook
     {
-
-
         public static void Postfix(ref WGText ___itemName)
         {
             ___itemName.GetComponent<Text>().alignment = TextAnchor.MiddleLeft;
@@ -176,12 +151,9 @@ namespace EnglishPatch.Hooks
         }
     }
 
-
     [HarmonyPatch(typeof(WGTitleValue), "TitleFormat")]
     public class WGTitleValue_TitleFormat_Hook
     {
-
-
         public static void Postfix(ref Text ___Title)
         {
             ___Title.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
@@ -189,12 +161,9 @@ namespace EnglishPatch.Hooks
         }
     }
 
-
     [HarmonyPatch(typeof(WGTitleValue), "ValueFromat")]
     public class WGTitleValue_ValueFromat_Hook
     {
-
-
         public static void Postfix(ref Text ___Value)
         {
             ___Value.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
@@ -202,13 +171,9 @@ namespace EnglishPatch.Hooks
         }
     }
 
-
-
     [HarmonyPatch(typeof(UIRelationship), "UpdateRelationship")]
     public class UIRelationship_UpdateRelationship_Hook
     {
-
-
         public static void Postfix(ref Text ___community_name, ref Text ___community_introduction)
         {
             ___community_name.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
@@ -220,6 +185,7 @@ namespace EnglishPatch.Hooks
      * Updating Date to display Year and Month in English
      * Updating the Units in SetMonth and adding a . after the number in both SetYear and SetMonth
     */
+
     [HarmonyPatch(typeof(UIDate), "SetYear")]
     public class UIDate_SetYear_Hook
     {
@@ -234,7 +200,6 @@ namespace EnglishPatch.Hooks
     {
         public static void Prefix(ref UIDate __instance, ref string monthStr)
         {
-
             monthStr = monthStr + ".";
 
             //Getting child 0, which is the RectTransform
@@ -269,44 +234,46 @@ namespace EnglishPatch.Hooks
             UnityEngine.UI.Text monthUnitText = monthUnit.gameObject.GetComponent<UnityEngine.UI.Text>();
             monthUnitText.text = "Month";
             monthUnitText.fontSize = 28;
-       }
+        }
     }
 
     /*
     * Do all MainUI manipulation in this function.
     */
-  //  [HarmonyPatch(typeof(UIMain), "Show")]
-   // public class UIMain_Show_Hook
-   // {
-     //   public static void Postfix(ref UIMain __instance)
-    //    {
-     //       RectTransform menu = (RectTransform)__instance.transform.Find("Menu");
-       //     //Updating the Credits Button Text
-         //   RectTransform btnMember = (RectTransform)menu.Find("BtnMember");
-           // RectTransform btn = (RectTransform)btnMember.Find("Btn");
-          //  RectTransform btn_text = (RectTransform)btn.Find("Text");
-         //   UnityEngine.UI.Text btn_text_text = (UnityEngine.UI.Text)btn_text.gameObject.GetComponentInChildren(typeof(UnityEngine.UI.Text));
-         //   btn_text_text.text = "Credits";
+
+    [HarmonyPatch(typeof(UIMain), "Show")]
+    public class UIMain_Show_Hook
+    {
+        public static void Postfix(ref UIMain __instance)
+        {
+            RectTransform dynamicCanvas = (RectTransform)__instance.transform.Find("DynamicCanvas");
+            RectTransform menu = (RectTransform)dynamicCanvas.transform.Find("Menu");
+            //Updating the Credits Button Text
+            RectTransform btnMember = (RectTransform)menu.Find("BtnMember");
+            RectTransform btn = (RectTransform)btnMember.Find("Btn");
+            RectTransform btn_text = (RectTransform)btn.Find("Text");
+            UnityEngine.UI.Text btn_text_text = (UnityEngine.UI.Text)btn_text.gameObject.GetComponentInChildren(typeof(UnityEngine.UI.Text));
+            btn_text_text.text = "Credits";
 
             /*
-            //Reducing Menu Button Font Size
-        //    menu.Find("BtnContinue").Find("Btn").Find("Text").GetComponent<Text>().fontSize = 20;
-       //     menu.Find("BtnNewGame").Find("Btn").Find("Text").GetComponent<Text>().fontSize = 23;
-       //     menu.Find("BtnLoadGame").Find("Btn").Find("Text").GetComponent<Text>().fontSize = 23;
-       //    menu.Find("BtnSystem").Find("Btn").Find("Text").GetComponent<Text>().fontSize = 16;
-        //    menu.Find("BtnQuitGame").Find("Btn").Find("Text").GetComponent<Text>().fontSize = 23;
+            Reducing Menu Button Font Size
+            menu.Find("BtnContinue").Find("Btn").Find("Text").GetComponent<Text>().fontSize = 20;
+             menu.Find("BtnNewGame").Find("Btn").Find("Text").GetComponent<Text>().fontSize = 23;
+             menu.Find("BtnLoadGame").Find("Btn").Find("Text").GetComponent<Text>().fontSize = 23;
+            menu.Find("BtnSystem").Find("Btn").Find("Text").GetComponent<Text>().fontSize = 16;
+            menu.Find("BtnQuitGame").Find("Btn").Find("Text").GetComponent<Text>().fontSize = 23;
             */
 
             //Focing the UI to update itself after all the changes we made. We just force every RectTransform to update itself
-            //for (int i = 0; i < __instance.transform.childCount; ++i)
-            //{
-            //    Transform t = __instance.transform.GetChild(i);
-            //    if (t != null && t.GetType() == typeof(RectTransform))
-            //    {
-            //        RectTransform rt = t as RectTransform;
-            //        rt.ForceUpdateRectTransforms();
-            //    }
-    //        }
-   //     }
-   // }
+            for (int i = 0; i < __instance.transform.childCount; ++i)
+            {
+                Transform t = __instance.transform.GetChild(i);
+                if (t != null && t.GetType() == typeof(RectTransform))
+                {
+                    RectTransform rt = t as RectTransform;
+                    rt.ForceUpdateRectTransforms();
+                }
+            }
+        }
+    }
 }
