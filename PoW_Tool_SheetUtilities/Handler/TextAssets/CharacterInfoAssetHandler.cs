56bcce14c874c05c40aeda2a66b19c2845aa0326
Magxm <19425588+Magxm@users.noreply.github.com>
2021-02-07 04:42:30 +0100

    Added CharacterInfoAssetHandler

--- PoW_Tool_SheetUtilities/Handler/TextAssets/CharacterInfoAssetHandler.cs
+++ PoW_Tool_SheetUtilities/Handler/TextAssets/CharacterInfoAssetHandler.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PoW_Tool_SheetUtilities.Handler.TextAssets
 {
-    class CharacterInfoAssetHandler : IFileHandler
+    internal class CharacterInfoAssetHandler : AssetHandler
     {
+        public CharacterInfoAssetHandler()
+        {
+            SheetId = "14sKOP4deJ6-e4_iBNac6MtOBjPtNAJKGXr-Ba6FBLXY";
+            AssetName = "CharacterInfo";
+            SheetRange = "A2:O";
+            FilePathWithoutExtension = "chs" + Path.DirectorySeparatorChar + "textfiles" + Path.DirectorySeparatorChar + "CharacterInfo";
+            OutputExtension = ".txt";
+
+            VariableDefinitions = new List<AssetVariableDefinition>()
+            {
+                new AssetVariableDefinition()
+                {
+                    Name = "ID",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Name",
+                    VariableType = AssetVariableType.Translate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "E",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "F",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "G",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "H",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "I",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "J",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "K",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "L",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "M",
+                    VariableType = AssetVariableType.Translate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "N",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "O",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+            };
+        }
     }
 }
