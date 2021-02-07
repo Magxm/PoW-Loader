98a99ff9b3b9aba3ab88b0f25d4259ee1e77d742
Magxm <19425588+Magxm@users.noreply.github.com>
2021-02-07 06:06:26 +0100

    Added NurturanceAssetHandler

--- PoW_Tool_SheetUtilities/Handler/TextAssets/NurturanceAssetHandler.cs
+++ PoW_Tool_SheetUtilities/Handler/TextAssets/NurturanceAssetHandler.cs
@@ -1,12 +1,112 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PoW_Tool_SheetUtilities.Handler.TextAssets
 {
-    class NurturanceAssetHandler : IFileHandler
+    internal class NurturanceAssetHandler : AssetHandler
     {
+        public NurturanceAssetHandler()
+        {
+            SheetId = "1BRSiGZcSrSQuBPuWEGkHxIeUtn97-jY1I8LDLeYrLy8";
+            AssetName = "Nurturance";
+            SheetRange = "A2:AB";
+            FilePathWithoutExtension = "chs" + Path.DirectorySeparatorChar + "textfiles" + Path.DirectorySeparatorChar + "Nurturance";
+            OutputExtension = ".txt";
+
+            VariableDefinitions = new List<AssetVariableDefinition>()
+            {
+                new AssetVariableDefinition()
+                {
+                    Name = "ID",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Name",
+                    VariableType = AssetVariableType.Translate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Description",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Function",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "UI Type",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Emotion",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Show Condition",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Open Condition",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Additional Condition",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Addition Value",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Tip",
+                    VariableType = AssetVariableType.Translate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Background",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "IconName",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Movie Number",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Not Open Tip",
+                    VariableType = AssetVariableType.Translate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Other Tip",
+                    VariableType = AssetVariableType.Translate
+                },
+                 new AssetVariableDefinition()
+                {
+                    Name = "Facility Tip",
+                    VariableType = AssetVariableType.Translate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Rewards",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+            };
+        }
     }
 }
