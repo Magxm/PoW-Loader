551c3f1f991a7ee3b84b13b45996435b409d3a24
Magxm <19425588+Magxm@users.noreply.github.com>
2021-02-08 06:45:34 +0100

    Added CharacterBehaviourAssetHandler

--- PoW_Tool_SheetUtilities/Handler/TextAssets/CharacterBehaviourAssetHandler.cs
+++ PoW_Tool_SheetUtilities/Handler/TextAssets/CharacterBehaviourAssetHandler.cs
@@ -1,12 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
+using System;
+using System.Collections.Generic;
+
+using System.IO;
 using System.Threading.Tasks;
 
 namespace PoW_Tool_SheetUtilities.Handler.TextAssets
 {
-    class CharacterBehaviourAssetHandler : IFileHandler
+    public class CharacterBehaviourAssetHandler : AssetHandler
     {
+        public CharacterBehaviourAssetHandler()
+        {
+            SheetId = "1RqJ9xAJ7HUU0H5b7S3LCrLLs2zDde1bYdc_7sQ9YBUY";
+            AssetName = "CharacterBehaviour";
+            SheetRange = "A2:P";
+            FilePathWithoutExtension = "chs" + Path.DirectorySeparatorChar + "textfiles" + Path.DirectorySeparatorChar + "CharacterBehaviour";
+            OutputExtension = ".txt";
+
+            VariableDefinitions = new List<AssetVariableDefinition>()
+            {
+                new AssetVariableDefinition()
+                {
+                    Name = "ID",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Remark",
+                    VariableType = AssetVariableType.MachineTL
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Position",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Rotation",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "IsTuen",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "TalkId",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "Animation",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "SchedulerId",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "ClickType",
+                    VariableType = AssetVariableType.NoTranslate
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "InteractiveName",
+                    VariableType = AssetVariableType.Translate,
+                },
+                 new AssetVariableDefinition()
+                {
+                    Name = "InteractiveEvent",
+                    VariableType = AssetVariableType.NoTranslate,
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "CreateCondition",
+                    VariableType = AssetVariableType.NoTranslate,
+                },
+                new AssetVariableDefinition()
+                {
+                    Name = "AppearCondition",
+                    VariableType = AssetVariableType.NoTranslate,
+                }
+            };
+        }
     }
 }
--- PoW_Tool_SheetUtilities/SpreadsheetUpdater.cs
+++ PoW_Tool_SheetUtilities/SpreadsheetUpdater.cs
@@ -15,6 +15,7 @@ namespace PoW_Tool_SheetUtilities
             string workingDirectory = Environment.CurrentDirectory;
             string inputFolder = workingDirectory + Path.DirectorySeparatorChar + "Input";
 
+            new CharacterBehaviourAssetHandler().UpdateSheetFromGameFile(inputFolder);
             new BookAssetHandler().UpdateSheetFromGameFile(inputFolder);
             new BattleAssetHandler().UpdateSheetFromGameFile(inputFolder);
             new TraitAssetHandler().UpdateSheetFromGameFile(inputFolder);
@@ -50,31 +51,46 @@ namespace PoW_Tool_SheetUtilities
             //Get input folder path
             string textAssetFolder = outputFolder + Path.DirectorySeparatorChar + "chs" + Path.DirectorySeparatorChar + "textfiles";
 
+            new CharacterBehaviourAssetHandler().BuildGameDataFromSheet(outputFolder);
+            Thread.Sleep(1000);
             new BookAssetHandler().BuildGameDataFromSheet(outputFolder);
+            Thread.Sleep(1000);
             new BattleAssetHandler().BuildGameDataFromSheet(outputFolder);
+            Thread.Sleep(1000);
             new TraitAssetHandler().BuildGameDataFromSheet(outputFolder);
+            Thread.Sleep(1000);
             new StringTableAssetHandler().BuildGameDataFromSheet(outputFolder);
+            Thread.Sleep(1000);
             new QuestAssetHandler().BuildGameDataFromSheet(outputFolder);
+            Thread.Sleep(1000);
             new PropsAssetHandler().BuildGameDataFromSheet(outputFolder);
+            Thread.Sleep(1000);
             new NurturanceAssetHandler().BuildGameDataFromSheet(outputFolder);
+            Thread.Sleep(1000);
             new EventCubeAssetHandler().BuildGameDataFromSheet(outputFolder);
+            Thread.Sleep(1000);
             new NpcAssetHandler().BuildGameDataFromSheet(outputFolder);
+            Thread.Sleep(1000);
             new CharacterInfoAssetHandler().BuildGameDataFromSheet(outputFolder);
+            Thread.Sleep(1000);
             new BattleAreaAssetHandler().BuildGameDataFromSheet(outputFolder);
+            Thread.Sleep(1000);
             new MantraAssetHandler().BuildGameDataFromSheet(outputFolder);
-
-            Console.WriteLine("Waiting 10 seconds because of Google API Quotas...");
-            Thread.Sleep(10000);
+            Thread.Sleep(1000);
 
             //OLD HANDLERS
             //Exporting TextAssets
             new TalkAssetHandler().BuildGameDataFromSheet(textAssetFolder);
+            Thread.Sleep(1000);
             new AchievementAssetHandler().BuildGameDataFromSheet(textAssetFolder);
+            Thread.Sleep(1000);
             new AdjustmentAssetHandler().BuildGameDataFromSheet(textAssetFolder);
+            Thread.Sleep(1000);
             new AlchemyAssetHandler().BuildGameDataFromSheet(textAssetFolder);
+            Thread.Sleep(1000);
             //new AnimationMappingAssetHandler().BuildGameDataFromSheet(textAssetFolder);
             new SkillAssetHandler().BuildGameDataFromSheet(textAssetFolder);
-
+            Thread.Sleep(1000);
             //Exporting battle related Assets
             string battleAssetFolder = outputFolder + Path.DirectorySeparatorChar + "chs" + Path.DirectorySeparatorChar + "battle";
             new BufferAssetHandler().BuildGameDataFromSheet(battleAssetFolder);
