a8bca88dbb821be5b87b196851f833a99ce965c9
Magxm <19425588+AndrasMumm@users.noreply.github.com>
2021-10-08 09:10:08 +0200

    Core for own Mod Settings window

---
+++ PoW_ModAPI/UI/Hooks/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+
+using Heluo.FSM.Main;
+
+namespace PoW_ModAPI.UI.Hooks
+{
+    public class MainMenuWrapper
+    {
+        //Singleton Stuff
+        private static MainMenuWrapper __instance = null;
+
+        public static MainMenuWrapper GetInstance()
+        {
+            if (__instance == null)
+            {
+                __instance = new MainMenuWrapper();
+            }
+
+            return __instance;
+        }
+
+        private MainMenuWrapper()
+        {
+        }
+
+        //Members
+        public MainMenu GameObject;
+    }
+
+    [HarmonyPatch(typeof(MainMenu), "OpenMain")]
+    public class MainMenu_OpenMain_Hook
+    {
+        public static bool Prefix(ref MainMenu __instance)
+        {
+            MainMenuWrapper.GetInstance().GameObject = __instance;
+            return true;
+        }
+    }
+}
---
+++ PoW_ModAPI/UI/ModSettings/ModSettings.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+
+using Heluo;
+using Heluo.Components;
+using Heluo.Controller;
+using Heluo.FSM.Main;
+using Heluo.UI;
+
+using PoW_ModAPI.UI.Hooks;
+
+namespace PoW_ModAPI.UI.ModSettings
+{
+    internal class ModSettings : UIForm
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        public override void Show()
+        {
+            GameCamera mainCamera = Game.MainCamera;
+            if (mainCamera != null)
+            {
+                mainCamera.Blur(false, 0f, null, 1.5f);
+            }
+
+            InputManager input = Game.Input;
+            if (input != null)
+            {
+                input.Push(this);
+            }
+
+            base.Show();
+        }
+
+        public override void Hide()
+        {
+            base.Hide();
+            InputManager input = Game.Input;
+            if (input != null)
+            {
+                input.Pop(this);
+            }
+
+            GameCamera mainCamera = Game.MainCamera;
+            if (mainCamera != null)
+            {
+                mainCamera.Blur(true, 0f, null, 1.5f);
+            }
+
+            //Now we need to go back to the Main menu
+            MainMenu mainMenu = MainMenuWrapper.GetInstance().GameObject;
+            MethodInfo openMenuMethodInfo = mainMenu.GetType().GetMethod("OpenMain", BindingFlags.NonPublic | BindingFlags.Instance);
+            openMenuMethodInfo.Invoke(mainMenu, new object[] { });
+        }
+    }
+}
