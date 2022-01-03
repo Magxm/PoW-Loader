a3408c0b5021eb38f3a845ff19bba2753ed483b4
Magxm <19425588+AndrasMumm@users.noreply.github.com>
2022-01-03 18:10:39 +0900

    Added batch file to prepare release (supports both winrar and 7zip)

---
+++ Output/PackRelease.bat
@@ -0,0 +1,24 @@
+@echo off
+rar > nul 2>&1
+if "%errorlevel%" == "9009" (
+    echo WinRar not found... Trying 7-Zip
+    goto :7zip
+) else (
+    winrar a -afzip PoW_English.zip Mod
+    winrar a -afzip QoL.zip Qol_Mod
+    goto :end
+)
+
+:7zip
+7z > nul 2>&1
+if "%errorlevel%" == "9009" (
+    echo 7-Zip not found... Please install either WinRar or 7-Zip and add their binary locations to your environment path
+) else (
+    7z a -tzip PoW_English.zip Mod
+    7z a -tzip QoL.zip Qol_Mod
+    goto :end
+)
+
+:end
+echo Done!
+START https://github.com/AndrasMumm/PoW-Loader/releases/new
