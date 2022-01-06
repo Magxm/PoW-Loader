92ac150b37ab2e44d580c081aed684909e6aec9c
Magxm <19425588+AndrasMumm@users.noreply.github.com>
2022-01-07 01:34:44 +0900

    Disabled Smooth Combat for summon skills and added animation callback handling

--- QuadruplicationOfLikeability/Hooks/Battle.cs
+++ QuadruplicationOfLikeability/Hooks/Battle.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
 using HarmonyLib;
-
 using Heluo.Actor;
 using Heluo.Animation;
 using Heluo.Battle;
 using Heluo.Events;
+using Heluo.Utility;
 using UnityEngine;
 
 namespace QoL.Hooks
@@ -67,10 +67,85 @@ namespace QoL.Hooks
 
             if (QoLMod.GetInstance().GetSmoothBattlesEnabled())
             {
-                //To make transitions smooth, we modify the wait time to be shorter than the actual animation
-                resultLength /= 1.5f;
+                //We check the stacktrace and make sure we only do this if the caller is not the "Process" method from the "SummonProcessStrategy" class.
+                StackTrace stackTrace = new StackTrace();
+                bool isSummonProcess = false;
+                for (int i = 0; i < stackTrace.FrameCount; i++)
+                {
+                    StackFrame frame = stackTrace.GetFrame(i);
+                    MethodBase method = frame.GetMethod();
+                    if (method.DeclaringType.Name != "SummonProcessStrategy")
+                    {
+                        isSummonProcess = true;
+                        break;
+                    }
+                }
+
+                if (!isSummonProcess)
+                {
+                    resultLength /= 1.5f;
+                }
             }
+
             __result = Task.FromResult(resultLength);
+            return false;
+        }
+    }
+
+    //We hook ActorController's "PlayCustom" to make sure the callback
+    //is called at the right time rather than the original one if Smooth battle and/or animation speed is enabled
+    [HarmonyPatch(typeof(ActorController), "PlayCustom", new Type[] { typeof(AnimationClip), typeof(float), typeof(Action) })]
+    public class ActorController_PlayCustom_Hook
+    {
+        public static void Prefix(ref ActorController __instance, ref AnimationClip clip, ref float time, ref Action callback)
+        {
+            if (QoLMod.GetInstance().GetSmoothBattlesEnabled())
+            {
+                time /= 1.5f;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(ActorController), "PlayCustom", new Type[] { typeof(AnimationClip), typeof(bool), typeof(Action) })]
+    public class ActorController_PlayCustom_Hook2
+    {
+        public static bool Prefix(ref ActorController __instance, ref AnimationClip clip, ref bool callStop, ref Action callback, ref AnimatorOverrideController ___controller, ref Action ___call_back, ref Timer ___timer, ref ActorLinkPoint ___actorlinkpoint)
+        {
+            //We override the whole function
+            ActorLinkPoint actorLinkPoint = ___actorlinkpoint;
+            if (actorLinkPoint != null)
+            {
+                actorLinkPoint.ResetPoint();
+            }
+            __instance.Animator.ResetTrigger(ActorController.Params.LeaveCustom);
+            __instance.Animator.ResetTrigger(ActorController.Params.Custom);
+            ___controller[ActorController.State.Custom.ToString().ToLower()] = clip;
+            __instance.Animator.SetTrigger(ActorController.Params.Custom);
+            ___call_back = callback;
+            if (callback != null)
+            {
+                if (___timer != null)
+                {
+                    ___timer.Stop();
+                }
+                var length = clip.length;
+                if (QoLMod.GetInstance().GetSmoothBattlesEnabled())
+                {
+                    length /= 1.5f;
+                }
+
+                if (callStop)
+                {
+                    ___timer = new Timer(length, new Action(__instance.StopCustomCallBack));
+                }
+                else
+                {
+                    ___timer = new Timer(length, callback);
+                }
+                ___timer.Start();
+            }
+
+            //Do not call original
             return false;
         }
     }
