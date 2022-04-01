6f5e9654de58e409f72b6db2acd09b55705b1915
Magxm <19425588+AndrasMumm@users.noreply.github.com>
2022-04-01 22:34:40 +0900

    Reworked Battle schedule handling

--- PoW_Tool_SheetUtilities/Handler/BattleAssets/BattleAssetHandler.cs
+++ PoW_Tool_SheetUtilities/Handler/BattleAssets/BattleAssetHandler.cs
@@ -1,57 +1,32 @@
 ﻿using Google.Apis.Sheets.v4;
 using Google.Apis.Sheets.v4.Data;
 
+using Newtonsoft.Json.Linq;
+
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace PoW_Tool_SheetUtilities.Handler.BattleAssets
 {
-    public class BattleAsset : AssetEntry
-    {
-        public void PopulateByGameAssetRow(string fileName, string[] row)
-        {
-            //Setting fileName
-            Variables[0].SetNewOriginal(fileName);
-            //Setting other variables
-            int columnIndex = 0;
-            for (int i = 1; i < VariableDefinitions.Count; i++) //Skipping filePath
-                Variables[i].SetNewOriginal(row[columnIndex++]);
-        }
-
-        public BattleAsset(List<AssetVariableDefinition> variableDefinitions)
-        {
-            VariableDefinitions = variableDefinitions;
-            Variables = new AssetVariable[VariableDefinitions.Count];
-            for (int i = 0; i < VariableDefinitions.Count; i++)
-            {
-                AssetVariableDefinition varDef = VariableDefinitions[i];
-                Variables[i] = new AssetVariable(varDef);
-            }
-        }
-
-        public new void AppendToFile(StreamWriter sw, AssetEntry thisEntry)
-        {
-            for (int i = 1; i < VariableDefinitions.Count; i++)
-            {
-                if (i > 1)
-                {
-                    sw.Write('\t');
-                }
-                Variables[i].AppendToFile(sw);
-            }
-
-            sw.Write('\r');
-        }
-    }
-
     public class BattleAssetHandler : AssetHandler
     {
+        private static string BattleResultChangeWinTipRegexPattern = "\"{ \\\"BattleResultChangeWinTip\\\" : \\\".*\\\"} \"";
+        private static Regex _BattleResultChangeWinTipRegex = new Regex(BattleResultChangeWinTipRegexPattern);
+
+        private static string BattleResultChangeLoseTipRegexPattern = "\"{ \\\"BattleResultChangeLoseTip\\\" : \\\".*\\\"} \"";
+        private static Regex BattleResultChangeLoseTipRegex = new Regex(BattleResultChangeLoseTipRegexPattern);
+
+        private static string BattleResultAddSecondaryGoalRegexPattern = "\"{ \\\"BattleResultAddSecondaryGoal\\\" : \\\".*\\\"} \"";
+        private static Regex BattleResultAddSecondaryGoalRegex = new Regex(BattleResultAddSecondaryGoalRegexPattern);
+
         public BattleAssetHandler()
         {
             SheetId = "1Rva4xUTKRovzb8rXKEwC7gzSFjCJVuUwVaP1rymGNi0";
             AssetName = "Battle";
-            SheetRange = "A2:O";
+            SheetRange = "A2:D";
             FilePathWithoutExtension = "chs" + Path.DirectorySeparatorChar + "battle" + Path.DirectorySeparatorChar + "merge" + Path.DirectorySeparatorChar + "Schedule";
             OutputExtension = ".txt";
 
@@ -59,96 +34,85 @@ namespace PoW_Tool_SheetUtilities.Handler.BattleAssets
             {
                 new AssetVariableDefinition()
                 {
-                    Name = "FileName",
-                    VariableType = AssetVariableType.NoTranslate
-                },
-                new AssetVariableDefinition()
-                {
-                    Name = "ID",
-                    VariableType = AssetVariableType.NoTranslate
-                },
-                new AssetVariableDefinition()
-                {
-                    Name = "Name",
-                    VariableType = AssetVariableType.MachineTL
-                },
-                new AssetVariableDefinition()
-                {
-                    Name = "BattleSchedules",
-                    VariableType = AssetVariableType.Translate,
-                    AutoML = false,
-                    IsScriptField_FilterNoText = true,
-                },
-                new AssetVariableDefinition()
-                {
-                    Name = "Remark",
-                    VariableType = AssetVariableType.MachineTL
-                },
-                new AssetVariableDefinition()
-                {
-                    Name = "WinTip",
+                    Name = "Translate",
                     VariableType = AssetVariableType.Translate
                 },
                 new AssetVariableDefinition()
                 {
-                    Name = "LoseTip",
-                    VariableType = AssetVariableType.Translate
-                },
+                    Name = "Type",
+                    VariableType = AssetVariableType.NoTranslate
+                }
             };
         }
 
-        public override void BuildGameDataFromSheet(string outRootPath)
+        public class ToPatchInstance
         {
-            string mergeFilePath = outRootPath + Path.DirectorySeparatorChar + FilePathWithoutExtension + OutputExtension;
-            Console.WriteLine("Getting " + AssetName + " Spreadsheet content");
+            public string ToPatch;
+            public string Type;
 
-            SpreadsheetsResource.ValuesResource.GetRequest request = GoogleSheetConnector.GetInstance().Service.Spreadsheets.Values.Get(SheetId, SheetRange);
-            ValueRange response = request.Execute();
-            List<IList<object>> values = (List<IList<object>>)response.Values;
-
-            //Clearing Asset File
-            string outDirectory = Path.GetDirectoryName(mergeFilePath);
-            if (!Directory.Exists(outDirectory))
+            public ToPatchInstance(string v, string t)
             {
-                Directory.CreateDirectory(outDirectory);
+                ToPatch = v;
+                Type = t;
             }
-            //Resetting file
-            File.WriteAllText(mergeFilePath, "");
 
-            string scheduleRelativeFolderPath = Path.DirectorySeparatorChar + "chs" + Path.DirectorySeparatorChar + "battle" + Path.DirectorySeparatorChar + "schedule";
-            string scheduleFolderPath = outRootPath + Path.DirectorySeparatorChar + scheduleRelativeFolderPath;
-
-            //Getting all Sheet entries and dumping them into Talk.txt in right format
-            Console.WriteLine("Extracting to " + FilePathWithoutExtension + OutputExtension + " and the schedules folder");
-            StreamWriter mergeFileWriter = File.AppendText(mergeFilePath);
-
-            if (values != null && values.Count > 0)
+            public int GetHashCode()
             {
-                foreach (var row in values)
+                unchecked // Overflow is fine, just wrap
                 {
-                    BattleAsset thisEntry = new BattleAsset(VariableDefinitions);
-                    thisEntry.PopulateBySheetRow(row);
-
-                    //Writing to merge file
-                    thisEntry.AppendToFile(mergeFileWriter, thisEntry);
-
-                    //Writting to .json file
-                    string fileName = thisEntry.Variables[0].OriginalValue;
-                    string filePath = scheduleFolderPath + Path.DirectorySeparatorChar + fileName;
-                    outDirectory = Path.GetDirectoryName(filePath);
-                    if (!Directory.Exists(outDirectory))
-                    {
-                        Directory.CreateDirectory(outDirectory);
-                    }
-
-                    File.WriteAllText(filePath, "");
-                    StreamWriter scheduleFileWriter = File.AppendText(filePath);
-                    thisEntry.AppendToFile(scheduleFileWriter, thisEntry);
-                    scheduleFileWriter.Close();
+                    int hash = 17;
+                    hash = hash * 23 + (ToPatch == null ? 0 : ToPatch.GetHashCode());
+                    hash = hash * 23 + (Type == null ? 0 : Type.GetHashCode());
+                    return hash;
                 }
             }
+        }
 
-            mergeFileWriter.Close();
+        private class ToPatchInstanceComparer : IEqualityComparer<ToPatchInstance>
+        {
+            public bool Equals(ToPatchInstance a, ToPatchInstance b)
+            {
+                return a.ToPatch == b.ToPatch && a.Type == b.Type;
+            }
+
+            public int GetHashCode(ToPatchInstance obj)
+            {
+                return obj.GetHashCode();
+            }
+        }
+
+        public List<ToPatchInstance> GetPatchableInstances(string data)
+        {
+            List<ToPatchInstance> patchables = new List<ToPatchInstance>();
+
+            Match m = _BattleResultChangeWinTipRegex.Match(data);
+            while (m.Success)
+            {
+                //Console.WriteLine("----------");
+                patchables.Add(new ToPatchInstance(m.Value, "WinTip"));
+
+                m = m.NextMatch();
+            }
+
+            m = BattleResultChangeLoseTipRegex.Match(data);
+            while (m.Success)
+            {
+                //Console.WriteLine("----------");
+                patchables.Add(new ToPatchInstance(m.Value, "LoseTip"));
+
+                m = m.NextMatch();
+            }
+
+            m = BattleResultAddSecondaryGoalRegex.Match(data);
+            while (m.Success)
+            {
+                //Console.WriteLine("----------");
+                patchables.Add(new ToPatchInstance(m.Value, "AddSecondaryGoal"));
+
+                m = m.NextMatch();
+            }
+
+            return patchables;
         }
 
         public override void UpdateSheetFromGameFile(string inputFolder)
@@ -156,10 +120,10 @@ namespace PoW_Tool_SheetUtilities.Handler.BattleAssets
             string scheduleFolderPath = inputFolder + Path.DirectorySeparatorChar + "chs" + Path.DirectorySeparatorChar + "battle" + Path.DirectorySeparatorChar + "schedule";
 
             //Getting all current entries
-            Dictionary<string, BattleAsset> entries = new Dictionary<string, BattleAsset>();
+            Dictionary<int, AssetEntry> entries = new Dictionary<int, AssetEntry>();
             GoogleSheetConnector gsc = GoogleSheetConnector.GetInstance();
 
-            Console.WriteLine("Getting " + AssetName + " Spreadsheet content");
+            Console.WriteLine("Getting " + AssetName + " Patching Spreadsheet content");
 
             SpreadsheetsResource.ValuesResource.GetRequest request = gsc.Service.Spreadsheets.Values.Get(SheetId, SheetRange);
             ValueRange response = request.Execute();
@@ -170,61 +134,169 @@ namespace PoW_Tool_SheetUtilities.Handler.BattleAssets
             {
                 foreach (var row in values)
                 {
-                    BattleAsset ae = new BattleAsset(VariableDefinitions);
+                    AssetEntry ae = new AssetEntry(VariableDefinitions);
                     ae.PopulateBySheetRow(row);
                     ae.Row = rowC;
+                    string text = (string)row[1];
+                    string type = (string)row[3];
+                    int hash = new ToPatchInstance(text, type).GetHashCode();
 
-                    entries[(string)row[0]] = ae;
+                    if (entries.ContainsKey(hash))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Detected multiple entries for text " + text + " in row " + (ae.Row + 1) + " ! Using last one...");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    entries[hash] = ae;
+
                     rowC++;
                 }
             }
 
             var updateRequests = new List<Request>();
-            Console.WriteLine("Comparing with games version and calculating updates...");
+            Console.WriteLine("Searching for important entries in battle schedule files...");
             //Parse every battle schedule file
             string[] files = Directory.GetFiles(scheduleFolderPath, "*.bytes");
+
+            List<ToPatchInstance> toPatch = new List<ToPatchInstance>();
             for (int i = 0; i < files.Length; i++)
             {
-                Console.Title = "[" + AssetName + "] Processing " + i + "/" + files.Length;
-
-                //Reading file
                 string scheduleFilePathTmp = files[i];
-                System.IO.StreamReader reader = new System.IO.StreamReader(scheduleFilePathTmp);
-                string line = reader.ReadLine();
 
                 //Construction relative .json path
                 int lastSplitterIndex = scheduleFilePathTmp.LastIndexOf(Path.DirectorySeparatorChar);
                 string scheduleFilePath = scheduleFilePathTmp.Substring(lastSplitterIndex + 1, scheduleFilePathTmp.Length - lastSplitterIndex - 1);
                 scheduleFilePath = scheduleFilePath.Replace(".bytes", ".json");
 
-                //Parsing
-                string[] data = line.Split('\t');
-                BattleAsset entry;
-                if (entries.ContainsKey(scheduleFilePath))
+                Console.Title = "[" + AssetName + "] Processing " + i + "/" + files.Length + " => " + scheduleFilePath;
+
+                //Reading file
+                string content = File.ReadAllText(scheduleFilePathTmp);
+
+                //Creating JSON object
+                try
+                {
+                    JObject obj = JObject.Parse(content);
+
+                    toPatch.Add(new ToPatchInstance((string)obj["WinTip"], "WinTip"));
+                    toPatch.Add(new ToPatchInstance((string)obj["LoseTip"], "LoseTip"));
+
+                    List<ToPatchInstance> toPatchRegexResult = GetPatchableInstances(content);
+                    foreach (ToPatchInstance v in toPatchRegexResult)
+                    {
+                        toPatch.Add(v);
+                    }
+                }
+                catch (Exception e)
+                {
+                    ///Might be outdated file/wrong format
+                    Console.WriteLine("Outdated or otherwise broken battle schedule file " + scheduleFilePath);
+                }
+            }
+
+            toPatch = toPatch.Distinct(new ToPatchInstanceComparer()).ToList();
+
+            Console.WriteLine("Patching...");
+            for (int i = 0; i < toPatch.Count; i++)
+            {
+                var e = toPatch[i];
+                if (e.ToPatch == null || e.ToPatch.Length == 0)
+                {
+                    continue;
+                }
+
+                Console.Title = "[" + AssetName + "] Patching " + i + "/" + toPatch.Count + " => " + e.ToPatch;
+                AssetEntry entry;
+                int hash = e.GetHashCode();
+                if (entries.ContainsKey(hash))
                 {
                     //Compare and update
-                    entry = entries[scheduleFilePath];
+                    entry = entries[hash];
                 }
                 else
                 {
                     //New entry
-                    entry = new BattleAsset(VariableDefinitions);
+                    entry = new AssetEntry(VariableDefinitions);
+                    entries[hash] = entry;
                 }
 
-                entry.PopulateByGameAssetRow(scheduleFilePath, data);
+                entry.PopulateByGameAssetRow(new string[] { e.ToPatch, e.Type });
                 List<Request> updateReqs = entry.GetUpdateRequests();
                 foreach (Request req in updateReqs)
                 {
                     updateRequests.Add(req);
                 }
 
-                if (updateRequests.Count >= 2500)
+                if (updateRequests.Count >= 10)
                 {
                     HandleUpdateRequests(ref updateRequests);
                 }
             }
-
             HandleUpdateRequests(ref updateRequests);
+        }
+
+        public override void BuildGameDataFromSheet(string outRootPath)
+        {
+            Console.WriteLine("Getting " + AssetName + " Spreadsheet content");
+
+            SpreadsheetsResource.ValuesResource.GetRequest request = GoogleSheetConnector.GetInstance().Service.Spreadsheets.Values.Get(SheetId, SheetRange);
+            ValueRange response = request.Execute();
+            List<IList<object>> values = (List<IList<object>>)response.Values;
+
+            List<AssetEntry> patches = new List<AssetEntry>();
+            if (values != null && values.Count > 0)
+            {
+                foreach (var row in values)
+                {
+                    AssetEntry thisEntry = new AssetEntry(VariableDefinitions);
+                    thisEntry.PopulateBySheetRow(row);
+                    patches.Add(thisEntry);
+                }
+            }
+            //Clearing Asset File
+            string scheduleRelativeFolderPath = Path.DirectorySeparatorChar + "chs" + Path.DirectorySeparatorChar + "battle" + Path.DirectorySeparatorChar + "schedule";
+            string scheduleFolderPath = outRootPath + Path.DirectorySeparatorChar + scheduleRelativeFolderPath;
+            string mergeFileInputPath = Environment.CurrentDirectory + Path.DirectorySeparatorChar + "Input" + Path.DirectorySeparatorChar + FilePathWithoutExtension + OutputExtension;
+            string mergeFileOutPath = outRootPath + Path.DirectorySeparatorChar + FilePathWithoutExtension + OutputExtension;
+            string outDirectory = Path.GetDirectoryName(scheduleFolderPath);
+            if (!Directory.Exists(scheduleFolderPath))
+            {
+                Directory.CreateDirectory(scheduleFolderPath);
+            }
+
+            //Resetting merge file
+            File.WriteAllText(mergeFileOutPath, "");
+
+            //We patch every input file
+            string inputFolderPath = Environment.CurrentDirectory + Path.DirectorySeparatorChar + "Input" + Path.DirectorySeparatorChar + "chs" + Path.DirectorySeparatorChar + "battle" + Path.DirectorySeparatorChar + "schedule";
+            Console.WriteLine("Patching battle schedule files from " + inputFolderPath + " into " + scheduleFolderPath);
+            string[] files = Directory.GetFiles(scheduleFolderPath, "*.bytes");
+            for (int i = 0; i < files.Length; i++)
+            {
+                string filePath = files[i];
+                Console.Title = "[" + AssetName + "] Processing " + i + "/" + files.Length + " => " + filePath;
+                string data = File.ReadAllText(filePath);
+
+                foreach (var patch in patches)
+                {
+                    data = data.Replace(patch.Variables[0].OriginalValue, patch.Variables[0].Translation);
+                }
+
+                string outFilePath = filePath.Replace(inputFolderPath, outDirectory);
+                outFilePath = outFilePath.Replace(".bytes", ".json");
+                File.WriteAllText(outFilePath, data);
+            }
+            Console.WriteLine("Patching merged battle schedule file!");
+            Console.Title = "[" + AssetName + "] Processing merged file...";
+            string content = File.ReadAllText(mergeFileInputPath);
+            foreach (var patch in patches)
+            {
+                content = content.Replace(patch.Variables[0].OriginalValue, patch.Variables[0].Translation);
+            }
+            File.WriteAllText(content, "");
+
+            Console.WriteLine("Done!");
+            Console.WriteLine("");
         }
     }
 }
