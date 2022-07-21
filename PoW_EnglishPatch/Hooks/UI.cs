f8676ff96ec5f59636d609ddc54d4876495f9fcf
Magxm <19425588+AndrasMumm@users.noreply.github.com>
2022-07-21 15:17:42 +0900

    Fixed UICommission_Show_Hook

--- PoW_EnglishPatch/Hooks/UI.cs
+++ PoW_EnglishPatch/Hooks/UI.cs
@@ -20,6 +20,7 @@ namespace EnglishPatch.Hooks
 
         public static void Postfix(ref WGText __instance, ref Text ___text)
         {
+            //Debug.Log("WGText_setText_Hook");
             ___text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
             ___text.fontSize = 20;
             if (___text.text.Length <= 20)
@@ -39,10 +40,11 @@ namespace EnglishPatch.Hooks
     {
         public static void Postfix(ref UICommission __instance)
         {
+            //Debug.Log("UICommission_Show_Hook");
             //Adjusting some elements to make the UI less scuffed (bc English texts are longer)
             RectTransform dynamicCanvas = (RectTransform)__instance.transform.Find("DynamicCanvas");
             RectTransform proclamation = (RectTransform)dynamicCanvas.transform.Find("Proclamation");
-            RectTransform limit = (RectTransform)proclamation.transform.Find("Limit");
+            Transform limit = (Transform)proclamation.transform.Find("Limit");
 
             //Updating the Credits Button Text
             RectTransform limit_Icon = (RectTransform)limit.Find("Limit_Icon");
@@ -60,6 +62,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Postfix(ref WGStringTable __instance)
         {
+            //Debug.Log("WGStringTable_UpdateString_Hook");
             __instance.GetComponent<Text>().font = Resources.GetBuiltinResource<Font>("Arial.ttf");
             if (__instance.GetComponent<Text>().text.Length <= 20)
             {
@@ -78,6 +81,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Postfix(ref UIBattleStatus __instance)
         {
+            //Debug.Log("UIBattleStatus_UpdateView_Hook");
             __instance.FinalCounterAttack.GetComponent<Text>().horizontalOverflow = HorizontalWrapMode.Overflow;
             __instance.FinalCrit.GetComponent<Text>().horizontalOverflow = HorizontalWrapMode.Overflow;
             __instance.FinalDodge.GetComponent<Text>().horizontalOverflow = HorizontalWrapMode.Overflow;
@@ -91,6 +95,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Postfix(ref UIQuest __instance, ref WGText ___questName, ref WGText ___description)
         {
+            //Debug.Log("UIQuest_SetTrackedQuest_Hook");
             ___questName.GetComponent<Text>().fontSize = 18;
             ___description.GetComponent<Text>().fontSize = 16;
         }
@@ -101,6 +106,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Postfix(ref WGBattleInfo __instance, ref WGText ___Name)
         {
+            //Debug.Log("WGBattleInfo_UpdateInfo_Hook");
             ___Name.GetComponent<Text>().font = Resources.GetBuiltinResource<Font>("Arial.ttf");
             ___Name.GetComponent<Text>().horizontalOverflow = HorizontalWrapMode.Overflow;
         }
@@ -111,6 +117,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Postfix(ref WGPaoMaDengText __instance, ref WGText ___pmdText)
         {
+            //Debug.Log("WGPaoMaDengText_AddMessage_Hook");
             ___pmdText.GetComponent<Text>().font = Resources.GetBuiltinResource<Font>("Arial.ttf");
         }
     }
@@ -120,6 +127,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Postfix(ref WGNurturanceBtn __instance, ref WGText ___Btn_Name)
         {
+            //Debug.Log("WGNurturanceBtn_UpdateButton_Hook");
             __instance.Btn_Name.GetComponent<Text>().font = Resources.GetBuiltinResource<Font>("Arial.ttf");
             __instance.Btn_Name.GetComponent<Text>().horizontalOverflow = HorizontalWrapMode.Wrap;
             __instance.Btn_Name.GetComponent<Text>().verticalOverflow = VerticalWrapMode.Overflow;
@@ -131,6 +139,7 @@ namespace EnglishPatch.Hooks
     {
         public static bool Prefix()
         {
+            //Debug.Log("UIRegistration_NameCheck_Hook");
             return false;
         }
     }
@@ -140,6 +149,7 @@ namespace EnglishPatch.Hooks
     {
         public static bool Prefix(string word, ref bool __result)
         {
+            //Debug.Log("SteamPlatform_CheckWordLegality_Hook");
             __result = true;
             return false;
         }
@@ -150,6 +160,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Postfix(ref WGText ___itemName)
         {
+            //Debug.Log("WGBookBtn_UpdateWidget_Hook");
             if (___itemName.GetComponent<Text>().text.Length <= 20)
             {
                 ___itemName.GetComponent<Text>().horizontalOverflow = HorizontalWrapMode.Overflow;
@@ -167,6 +178,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Postfix(ref WGText ___itemName)
         {
+            //Debug.Log("WGTraitBtn_UpdateWidget_Hook");
             ___itemName.GetComponent<Text>().alignment = TextAnchor.MiddleLeft;
             ___itemName.GetComponent<Text>().rectTransform.offsetMin = new Vector2(-120f, -20f);
         }
@@ -177,6 +189,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Postfix(ref Text ___Title)
         {
+            //Debug.Log("WGTitleValue_TitleFormat_Hook");
             ___Title.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
             ___Title.fontSize = 24;
         }
@@ -187,6 +200,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Postfix(ref Text ___Value)
         {
+            //Debug.Log("WGTitleValue_ValueFromat_Hook");
             ___Value.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
             ___Value.fontSize = 20;
         }
@@ -197,6 +211,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Postfix(ref Text ___community_name, ref Text ___community_introduction)
         {
+            //Debug.Log("UIRelationship_UpdateRelationship_Hook");
             ___community_name.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
             ___community_introduction.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
         }
@@ -212,6 +227,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Prefix(ref string yearStr)
         {
+            //Debug.Log("UIDate_SetYear_Hook");
             yearStr += ".";
         }
     }
@@ -221,6 +237,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Prefix(ref UIDate __instance, ref string monthStr)
         {
+            //Debug.Log("UIDate_SetMonth_Hook");
             monthStr += ".";
 
             //Getting child 0, which is the RectTransform
@@ -267,6 +284,7 @@ namespace EnglishPatch.Hooks
     {
         public static void Postfix(ref UIMain __instance)
         {
+            //Debug.Log("UIMain_Show_Hook");
             RectTransform dynamicCanvas = (RectTransform)__instance.transform.Find("DynamicCanvas");
             RectTransform menu = (RectTransform)dynamicCanvas.transform.Find("Menu");
             //Updating the Credits Button Text
