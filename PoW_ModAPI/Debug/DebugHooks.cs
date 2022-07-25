543f640e2bc2bf4498874fef3ff65efd1f047d1a
Magxm <19425588+AndrasMumm@users.noreply.github.com>
2022-07-25 17:53:59 +0900

    Added missing file

---
+++ PoW_ModAPI/Debug/DebugHooks.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+using Heluo.Battle;
+
+using UnityEngine;
+
+namespace PoW_ModAPI.DebugHooks
+{
+    [HarmonyPatch(typeof(WuxiaBattleSchedule), "InitBattleScheduleData")]
+    public class WuxiaBattleSchedule_InitBattleScheduleData_Hook
+    {
+        public static bool Prefix(string ScheduleID)
+        {
+            Debug.LogError("Loading BattleSchedule " + ScheduleID);
+            return true;
+        }
+    }
+}
